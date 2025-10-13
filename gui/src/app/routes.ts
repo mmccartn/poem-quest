fb414f6648a6ec288efd1a3d42691d66cc4704dd
Matt McCartney <mattsmccartney@gmail.com>
2025-10-13 15:58:08 -0700

    feat(gui): inline poem details view in the home page instead of separate

--- gui/src/app/routes.ts
+++ gui/src/app/routes.ts
@@ -1,18 +1,12 @@
 import { Routes } from '@angular/router'
 import { Home } from './home/home'
 import { PageNotFound } from './page-not-found/page-not-found'
-import { PoemDetails } from './poem-details/poem-details'
 
 const routeConfig: Routes = [
   {
     path: '',
     component: Home,
     title: 'PoemQuest'
-  },
-  {
-    path: 'details/:author/:title',
-    component: PoemDetails,
-    title: 'Poem details'
   },
   {
     path: '**',
